65fa9bfd34c1531f6dfb1d7923419f874e5e60e4
Marco Varrone <marco.varrone@mail.polimi.it>
2017-10-19 00:18:50 +0200

    Allow sequence initialization with list of bases/amino acids

--- sequence.py
+++ sequence.py
@@ -16,17 +16,30 @@ class Sequence:
         self.alphabet = alphabet
         if all(isinstance(element, Base) for element in sequence):
             self.sequence = sequence
+        elif all(isinstance(element, str) for element in sequence):
+            self.sequence = list(map(lambda x: alphabet[x](), sequence))
         elif isinstance(sequence, str):
             if alphabet is None:
                 self.sequence = sequence
             else:
                 self.sequence = str_to_sequence(sequence, alphabet)
+        else:
+            raise ValueError('Invalid sequence.')
+
+    def __getitem__(self, key):
+        return self.sequence[key]
+
+    def __len__(self):
+        return len(self.sequence)
 
     def __add__(self, other):
-        if(self.__class__ == other.__class__):
+        if self.__class__ == other.__class__:
             return Sequence(str(self) + str(other), self.alphabet)
         return False
 
-    def __repr__(self):
+    def __str__(self):
+
         seq_str = map(lambda x: str(x), self.sequence)
         return "".join(seq_str)
+
+
