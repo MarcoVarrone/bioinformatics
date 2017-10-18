d63bcc9ce0d6d4808bbb18e661f24a7a1b934a01
Marco Varrone <marco.varrone@mail.polimi.it>
2017-10-19 00:19:27 +0200

    Add complements to DNA

--- dna.py
+++ dna.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from sequence import Sequence
 from nucleic.adenine import Adenine
 from nucleic.cytosine import Cytosine
@@ -5,8 +7,25 @@ from nucleic.thymine import Thymine
 from nucleic.guanine import Guanine
 
 alphabet = {'A': Adenine, 'C': Cytosine, 'G': Guanine, 'T': Thymine}
+complements = {Adenine: Guanine, Cytosine: Thymine, Guanine: Adenine, Thymine: Cytosine}
+
 
 
 class DNA(Sequence):
     def __init__(self, sequence):
         super(DNA, self).__init__(sequence, alphabet)
+
+    def prova(self):
+        return self.sequence
+
+    def complement(self):
+        return DNA(list(map(lambda x: complements[x.__class__](), self.sequence)))
+
+    def reverse(self):
+        return DNA(str(self)[::1])
+
+
+
+    def __str__(self):
+        return super(DNA, self).__str__()
+
